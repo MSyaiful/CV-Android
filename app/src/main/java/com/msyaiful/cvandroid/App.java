4a983cd81791b6f647756c545fd3531c1aebb21d
Muhammad Syaiful Iskandar <msyaifuliskandar@gmail.com>
2019-08-28 13:53:41 +0700

    CV Android
    CV Android Pribadi

---
+++ app/src/main/java/com/msyaiful/cvandroid/App.java
@@ -0,0 +1,32 @@
+package com.msyaiful.cvandroid;
+
+public class App {
+    private String code_name;
+    private String summary;
+    private String photo;
+
+    public String getName() {
+        return code_name;
+    }
+
+    public void setName(String code_name) {
+        this.code_name = code_name;
+    }
+
+    public String getFrom() {
+        return summary;
+    }
+
+    public void setFrom(String summary) {
+        this.summary = summary;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
+
